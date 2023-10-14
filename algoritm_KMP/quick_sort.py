c59a645af0e480a0746d7bf266d56eb217dd5d1d
alexmais <alexlove9596@gmail.com>
2023-10-14 20:16:07 +0300

    add time work and random list with 100000 numbers

--- algoritm_KMP/quick_sort.py
+++ algoritm_KMP/quick_sort.py
@@ -1,11 +1,26 @@
-def choise_sort(lis_t):
-    if len(lis_t) < 2:
+import random
+import time
+
+
+def quick_sort(lis_t):
+    if len(lis_t) <= 1:
         return lis_t
     else:
-        base = lis_t[0]
-        lover = [i for i in lis_t[1:] if i <= base]
-        higher = [i for i in lis_t[1:] if i > base]
-        return choise_sort(lover) + [base] + choise_sort(higher)
+        bace = lis_t[0]
+        lover = [i for i in lis_t[1:] if i <= bace]
+        higher = [i for i in lis_t[1:] if i > bace]
+        return quick_sort(lover) + [bace] + quick_sort(higher)
 
 
-print(choise_sort([2, 5, 3, 7, 1, 1]))
+def main(list_t):
+    start = time.time()
+    print(quick_sort(lis_t))
+    end = time.time() - start
+
+    return end
+
+
+lis_t = [i for i in range(random.randint(1, 100000))]
+random.shuffle(lis_t)
+print(lis_t)
+print(main(lis_t))
